485314afdbb74297d5d53d7f54ba574651eb9464
eidosxxvi <momopeachboy026@gmail.com>
2016-01-15 22:38:58 -0800

    Revert "AutoOp Update"
    This reverts commit 7cc97d1d47a4919d198274d6f045ca5e5df53a9d.

--- FtcRobotController/src/main/java/com/whs542/ftc2016/AutoOp.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016/AutoOp.java
@@ -1,9 +1,5 @@
 package com.whs542.ftc2016;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 /**
  * Created by DanielWang on 12/9/15.
  */
@@ -26,107 +22,63 @@ public class AutoOp extends RobotMain
         telemetry.addData("state", state);
 
         //Note that if you make the power too big, it'll go way too fast and overshoot
-/*
-        switch(state)
-        {
-            case 0:
-                drive.setLeftRightPower(0.1, 0.1);
-                if(drive.hasTargetHit(1.0)) //value to be determined
-                {
-                    state = 1;
-                }
-                break;
-
-            case 1:
-                //Turn left towards beacon
-                drive.setLeftRightPower(0.0, 0.1);
-                drive.updateEncoderValues;
-                if(drive.hasTargetHit(1.0))
-                {
-                    state = 2;
-                }
-                break;
-
-            case 2:
-                //Drive forward towards beacon
-                drive.setLeftRightPower(0.1, 0.1);
-                if(drive.hasTargetHit(0.5))
-                {
-                    state = 3;
-                }
-                break;
-
-            case 3:
-                //Stop drive and switch beacon
-                drive.setLeftRightPower(0.0, 0.0);
-                if(//beacon has been switched)
+        
+            switch(state)
             {
-                state = 4;
-            }
-            break;
-
-            case 4:
-                //Back up from beacon and angle to drop climbers in shelter
-                drive.setLeftRightPower(-0.05, -0.1);
-                if(drive.hasTargetHit(1.0))
-                {
-                    drive.setLeftRightPower(0.1, 0.1); //maybe just make this a separate case itself
+                case 0:
+                    drive.setLeftRightPower(0.1, 0.1);
+                    if(drive.hasTargetHit(1.0))
                     {
-                        if(drive.hasTargetHit(0.5))
-                        {
-                            state = 5;
-                        }
+                        state = 1;
                     }
-                }
+                    break;
+
+                case 1:
+                    drive.setLeftRightPower(-0.1, -0.1);
+                    if(drive.hasTargetHit(1.0))
+                    {
+                        state = 2;
+                    }
+                    break;
+
+                case 2:
+                    drive.setLeftRightPower(0.1, 0.1);
+                    if(drive.hasTargetHit(1.0))
+                    {
+                        state = 3;
+                    }
+                    break;
+
+                case 3:
+                    drive.setLeftRightPower(-0.1, -0.1);
+                    if(drive.hasTargetHit(1.0))
+                    {
+                        state = 4;
+                    }
                 break;
 
-            case 5:
-                //Drop climbers into shelter
-                drive.setLeftRightPower(0.0, 0.0);
-                linearSlides.setExtension(true);
-                //Current sensors check how far it has gone
-                //Color sensing code
-                if(//beacon is switched)
-            {
-                state = 6;
+                case 4:
+                    drive.setLeftRightPower(0.1, 0.1);
+                    if(drive.hasTargetHit(1.0))
+                    {
+                        state = 5;
+                    }
+                    break;
+
+                case 5:
+                        drive.setLeftRightPower(0.0, 0.0);
+                    break;
             }
 
-            case 6:
-                //Back up from beacon and angle towards mountain
-                drive.setLeftRightPower(-0.1, 0.0);
-                if(drive.hasTargetHit(0.75))
-                {
-                    state = 7;
-                }
-                break;
-
-            case 7:
-                //Move towards mountain and climb onto midzone
-                drive.setLeftRightPower(0.1, 0.1);
-                if(drive.hasTargetHit(1.0)))
-            {
-                state = 8;
-            }
-            break;
-
-            case 8:
-                //Stop robot
-                drive.setLeftRightPower(0.0, 0.0);
-                break;
-
-        }
-*/
         //Lucy's Code//
-        HardwareMap driveMap = new HardwareMap();
-        DcMotor rightFrontMotor = driveMap.dcMotor.get("drive_rf");
-        rightFrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        control.speedUpMotor(rightFrontMotor, 1.0);
-        double power1 = rightFrontMotor.getPower();
+        /*
+        control.speedUpMotor(drive.rightFrontMotor, 1.0);
+        double power1 = drive.rightFrontMotor.getPower();
         //control.slowDownMotor(drive.rightFrontMotor);
 
 
         telemetry.addData("Motor power: ", power1);
-
+        */
     }
 
 }
--- FtcRobotController/src/main/java/com/whs542/ftc2016/subsys/SpeedControl.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016/subsys/SpeedControl.java
@@ -16,17 +16,17 @@ public class SpeedControl
 
     }
 
-    public void slowMotorLinearly(DcMotor motor) {
+    public void slowMotorLinearly(DcMotor motorToSlowDown)
+    {
         //Slow down which motor you want to slow down
-        power = motor.getPower();
+        power = motorToSlowDown.getPower();
 
-        while (Math.abs(power) > 0.0000001)
+        while(Math.abs(power) > 0.0000001)
         {
-            power *= .8;
-            motor.setPower(power);
+            motorToSlowDown.setPower(power/2);
         }
 
-        motor.setPower(0);
+        motorToSlowDown.setPower(0);
     }
 
     public void trapezoidalVelocity()
@@ -39,17 +39,15 @@ public class SpeedControl
 
     }
 
-    public static void speedUpMotor(DcMotor motor, double desiredPower)
+    public static void speedUpMotor(DcMotor motorToSpeedUp, double powerYouWant)
     {
         //Speed up the motor you want to speed up
-        double power = motor.getPower();
-        while(Math.abs(power) < Math.abs(desiredPower))
+        double currentPower = motorToSpeedUp.getPower();
+        while(currentPower < powerYouWant)
         {
-            power *= 1.3;
-            motor.setPower(power);
+            motorToSpeedUp.setPower(currentPower * 1.3);
         }
 
-        motor.setPower(desiredPower);
-
+        motorToSpeedUp.setPower(powerYouWant);
     }
 }
