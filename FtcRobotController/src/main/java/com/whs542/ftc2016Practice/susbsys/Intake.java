64490e20c8991bae2c20bd415bdae301660c73c1
Amar G <amargandhi10@gmail.com>
2016-04-27 17:15:10 -0700

    Intake Program

---
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/Intake.java
@@ -0,0 +1,32 @@
+package com.whs542.ftc2016Practice.susbsys;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+/**
+ * Created by Amar on 4/27/2016.
+ */
+public class Intake {
+    DcMotor intake;
+
+    public Intake (HardwareMap inMap){
+
+        intake = inMap.dcMotor.get("intake");
+
+    }
+
+    public void useIntake(boolean on, boolean reverse){
+
+        if(on){
+            intake.setPower(1.0);
+        }
+        else if (reverse){
+            intake.setPower(-1.0);
+        }
+        else{
+            intake.setPower(0.0);
+        }
+
+    }
+}
