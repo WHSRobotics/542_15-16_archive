d297016f787f1b98fe75cc1a1162f0d007e0eec0
Amar G <amargandhi10@gmail.com>
2016-04-30 11:23:32 -0700

    Scoring Mech Update
    An update, since I realized that my old version doesn't work at all.

--- FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/ScoringMechanism.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/ScoringMechanism.java
@@ -7,21 +7,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by Joyce on 4/29/2016.
  */
 public class ScoringMechanism {
+
     public static final double ENCODER_TICKS = ;
     DcMotor motor;
+
     public ScoringMechanism(HardwareMap scoringMap) {
-        motor = scoringMap.dcMotor.get("motor");
+        motor = scoringMap.dcMotor.get("scoringMechanism");
     }
     public void run(boolean forwards, boolean backwards){
-        if (motor.getCurrentPosition()< ENCODER_TICKS/3) {
-            if(forwards){
-                motor.setPower(0.5);
-            }else if(backwards){
-                motor.setPower(-0.5);
-            }else{
-                motor.setPower(0.0);
-            }
-
+        if (motor.getCurrentPosition()< ENCODER_TICKS/3 && forwards) {
+            motor.setPower(0.5);
+        }else if(motor.getCurrentPosition()>0 && backwards){
+            motor.setPower(-0.5);
         }else{
             motor.setPower(0.0);
         }
