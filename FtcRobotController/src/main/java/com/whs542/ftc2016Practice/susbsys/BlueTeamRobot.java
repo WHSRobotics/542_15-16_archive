575fe59cd1b5d18be38fedc35b406b9c822c022f
Amar G <amargandhi10@gmail.com>
2016-04-29 21:34:27 -0700

    Robot Subsys Class
    A class for creating a BlueTeamRobot object.

---
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/BlueTeamRobot.java
@@ -0,0 +1,16 @@
+package com.whs542.ftc2016Practice.susbsys;
+
+
+/**
+ * Created by Amar2 on 4/29/2016.
+ */
+public class BlueTeamRobot {
+
+    Drivetrain drivetrain;
+    Intake intake;
+    ScoringMechanism scoringMechanism;
+
+
+
+
+}
