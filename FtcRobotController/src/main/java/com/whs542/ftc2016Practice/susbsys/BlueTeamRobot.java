fe3fc4e756c67e7a1711e54144124edc5de5742a
Amar G <amargandhi10@gmail.com>
2016-04-29 22:20:38 -0700

    BlueTeamRobot changes
    Added constructor and Bno055 gyro.

--- FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/BlueTeamRobot.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/susbsys/BlueTeamRobot.java
@@ -1,5 +1,7 @@
 package com.whs542.ftc2016Practice.susbsys;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.whs542.lib.Wire.*;
 
 /**
  * Created by Amar2 on 4/29/2016.
@@ -10,7 +12,13 @@ public class BlueTeamRobot {
     Intake intake;
     ScoringMechanism scoringMechanism;
 
+    Bno055 gyro;
 
-
+    public BlueTeamRobot(HardwareMap mainMap){
+        drivetrain = new Drivetrain(mainMap);
+        intake = new Intake(mainMap);
+        scoringMechanism = new ScoringMechanism(mainMap);
+        gyro = new Bno055(mainMap, "gyro");
+    }
 
 }
