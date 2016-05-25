58e5f061785fa85c6e9855bae93cab52915df6f8
Joyce-Yuan <joyceyuan80@gmail.com>
2016-05-25 16:45:22 -0700

    AutoOp changes
    AutoOp is basically complete. Comments have also been added.

--- FtcRobotController/src/main/java/com/whs542/ftc2016Practice/AutoOp.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/AutoOp.java
@@ -37,15 +37,15 @@ public class AutoOp extends OpMode{
     **/
 
     @Override
-    public void loop()
+    public void loop()  //Robot starts at top left corner of floor mat next to loading zone
     {
         switch (i)
         {
             case(1):
-                robot.drivetrain.moveAuto(63.5,1.0);
+                robot.drivetrain.moveAuto(63.5,1.0); //robot center lines with center of target crate
                 i=2;
             case(2):
-                robot.drivetrain.setStartingDeg(robot.gyro.eulerZ());
+                robot.drivetrain.setStartingDeg(robot.gyro.eulerZ()); //turns toward crate
             case(3):
                 if(!robot.drivetrain.turn(90,false,0.5,robot.gyro.eulerZ())){
                 }
@@ -53,12 +53,18 @@ public class AutoOp extends OpMode{
                     i=4;
                 }
             case(4):
-                robot.drivetrain.moveAuto(48,1.0);
+                robot.drivetrain.moveAuto(30, 1.0);    //robot drives up to crate
                 i=5;
             case(5):
-                robot.drivetrain.moveAuto(48,0.5);
-                robot.scoringMechanism.flipForwards();
-
+                robot.drivetrain.moveAutoDecrease(41,1.0);   //robot pushes crate, slowly decreasing speed, until parking in opponent's zone
+                i=6;
+            case(6):
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                robot.scoringMechanism.flipForwards();   //robot drops the 2 waffles
         }
     }
 
