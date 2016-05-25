9b4d049429f12f50a27b5a3b88d073d59bd1ea17
Amar G <amargandhi10@gmail.com>
2016-05-25 16:03:03 -0700

    AutoOp Changes
    Added case(3) so that the robot can turn, because Joyce did not read my
    example.

--- FtcRobotController/src/main/java/com/whs542/ftc2016Practice/AutoOp.java
+++ FtcRobotController/src/main/java/com/whs542/ftc2016Practice/AutoOp.java
@@ -14,6 +14,10 @@ public class AutoOp extends OpMode{
         robot = new BlueTeamRobot(hardwareMap);
     }
 
+    ////////////////////////////
+    //---READ THIS JOYCE!!!---//
+    ////////////////////////////
+
     /*
     Example:
     @Override
@@ -41,12 +45,17 @@ public class AutoOp extends OpMode{
                 robot.drivetrain.moveAuto(63.5,1.0);
                 i=2;
             case(2):
-                robot.drivetrain.turn(90,false,0.5,robot.gyro.eulerZ());
-                i=3;
+                robot.drivetrain.setStartingDeg(robot.gyro.eulerZ());
             case(3):
-                robot.drivetrain.moveAuto(48,1.0);
-                i=4;
+                if(!robot.drivetrain.turn(90,false,0.5,robot.gyro.eulerZ())){
+                }
+                else{
+                    i=4;
+                }
             case(4):
+                robot.drivetrain.moveAuto(48,1.0);
+                i=5;
+            case(5):
                 robot.drivetrain.moveAuto(48,0.5);
                 robot.scoringMechanism.flipForwards();
 
